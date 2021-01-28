8e6ea1d018edcd8e27237985f440cea0c3178a01
Gérald Moirod <stonnersdu39@gmail.com>
2021-01-28 23:38:24 +0100

    travail bottomNavigation

--- app/src/main/java/fr/univ_lorraine/iutmetz/wmce/dmcd0/MainActivity.java
+++ app/src/main/java/fr/univ_lorraine/iutmetz/wmce/dmcd0/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
