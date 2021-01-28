a4010d4be481e785b96ca63232b4cddce487042b
Gérald Moirod <stonnersdu39@gmail.com>
2021-01-28 14:21:46 +0100

    travail connexion

--- app/src/main/java/fr/univ_lorraine/iutmetz/wmce/dmcd0/MainActivity.java
+++ app/src/main/java/fr/univ_lorraine/iutmetz/wmce/dmcd0/MainActivity.java
@@ -16,12 +16,16 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    SessionManager sessionManager;
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sessionManager = new SessionManager(this);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
--- app/src/main/java/fr/univ_lorraine/iutmetz/wmce/dmcd0/ui/ConnexionFragment.java
+++ app/src/main/java/fr/univ_lorraine/iutmetz/wmce/dmcd0/ui/ConnexionFragment.java
@@ -6,19 +6,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import fr.univ_lorraine.iutmetz.wmce.dmcd0.R;
+import fr.univ_lorraine.iutmetz.wmce.dmcd0.SessionManager;
 
 public class ConnexionFragment extends Fragment {
-
+SessionManager sessionManager;
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_connexion, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         textView.setText("Connexion !");
+        sessionManager= new SessionManager(this.getContext());
+
         return root;
     }
 }
