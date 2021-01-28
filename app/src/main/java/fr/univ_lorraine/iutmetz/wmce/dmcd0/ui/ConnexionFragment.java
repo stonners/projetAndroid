3248360d21cd2cdb6c7fb74726ab14345b70c3d2
Gérald Moirod <stonnersdu39@gmail.com>
2021-01-28 21:29:48 +0100

    travail connexion

--- app/src/main/java/fr/univ_lorraine/iutmetz/wmce/dmcd0/ui/ConnexionFragment.java
+++ app/src/main/java/fr/univ_lorraine/iutmetz/wmce/dmcd0/ui/ConnexionFragment.java
@@ -1,9 +1,11 @@
 package fr.univ_lorraine.iutmetz.wmce.dmcd0.ui;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,6 +14,7 @@ import fr.univ_lorraine.iutmetz.wmce.dmcd0.SessionManager;
 
 public class ConnexionFragment extends Fragment {
 SessionManager sessionManager;
+public Button btnConnexion;
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_connexion, container, false);
@@ -19,6 +22,14 @@ SessionManager sessionManager;
         textView.setText("Connexion !");
         sessionManager= new SessionManager(this.getContext());
 
+        this.btnConnexion = root.findViewById(R.id.btnConnexion);
+        this.btnConnexion.setOnClickListener(this::onConnexion);
         return root;
     }
+
+    private void onConnexion(View view) {
+        Log.e( "onConnexion: ","testttttttttttt" );
+    }
+
+
 }
